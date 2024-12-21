52f2d9aae1988d837a469c2079c810dc5045fdf8
olli <info@oliverlahr.de>
2024-12-21 16:00:39 +0100

    Add component to download calendar as `ICS`

---
+++ src/lib/components/features/download-button.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import IconDownload from "../shared/icon-download.svelte";
+
+  export let background = "bg-secondary";
+  export let textColor = "text-white";
+</script>
+
+<div class="flex items-center gap-4">
+  <button
+    class="
+        flex
+        items-center
+        gap-2
+        rounded-full
+        {background} 
+        {textColor} 
+        text-[16px]
+        py-3
+        px-5
+        shadow-md
+        ring-1
+        ring-black/5
+        transition-all
+        duration-300
+        ease-in-out
+        hover:scale-105"
+  >
+    <IconDownload /> Download ICS
+  </button>
+</div>
